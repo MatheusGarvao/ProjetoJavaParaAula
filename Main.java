78bf2ed977ba0e110c9e04a25297c6a0f6e71b5a
Matheus Garvão <100000464+MatheusGarvao@users.noreply.github.com>
2024-11-06 17:23:47 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+import contas.Conta;
+import contas.PF;
+
 public class Main {
     public static void main(String[] args) {
 
-        Banco[] bancos = new Banco[2];
+        bancos.Banco[] bancos = new bancos.Banco[2];
 
-        bancos[0] = new Banco(0, "Itau");
-        bancos[1] = new Banco(1, "Banco do Brasil");
+        bancos[0] = new bancos.Banco(0, "Itau");
+        bancos[1] = new bancos.Banco(1, "bancos.Banco do Brasil");
 
+        bancos[1].criarConta("123.456.789-01","PF");
+        Conta conta1 = bancos[1].localizarConta("123.456.789-01", "PF");
 
+        bancos[0].criarConta(conta1.getCPFTitular(), "PJ");
 
-
+        System.out.println(bancos[0].localizarConta("123.456.789-01","PJ").getTipoConta());
 
 
 
